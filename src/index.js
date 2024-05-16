75e37ad8f7f8e6757892449577bfa05f187ca2c3
EngelsGB <engelsgarciabello@gmail.com>
2024-05-16 16:24:57 -0400

    finished

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
  *
  */
 
-const handleSubmit = () => {
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    document.getElementById("results-username").textContent = form.username.value;
+    document.getElementById("results-coding-level").textContent = form.codingLevel.value;
+    document.getElementById('results-location').textContent = form.location.value;
+    form.didLikeAssignment.checked ? document.getElementById('results-did-like-assignment').textContent = "Yes" : document.getElementById('results-did-like-assignment').textContent = "No";
+    form.reset();
 };
+
+const main = () => {
+    document.querySelector('#new-user-form').addEventListener('submit', handleSubmit);
+}
+
+main();
